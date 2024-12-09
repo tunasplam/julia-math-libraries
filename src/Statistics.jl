1891b603c0ad6dfe30f5ec1431f2692bf9ece64c
tunasplam <tunasplam@gmail.com>
2024-12-08 22:29:01 -0800

    More robust typing

--- src/Statistics.jl
+++ src/Statistics.jl
@@ -2,11 +2,11 @@
     Basic statistics go here
 =#
 
-function variance(l)
+function variance(l::Vector{T}) where T<:Real
     # NOTE this is thousands of times faster than traditional approach
     return mean(map((x) -> x^2, l)) - mean(l)^2
 end
 
-function mean(l)
+function mean(l::Vector{T}) where T<:Real
     return sum(l) / length(l)
 end
