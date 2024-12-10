8cd6cb71f0af59a5525eb660c74d7a3e943e7f13
tunasplam <tunasplam@gmail.com>
2024-12-09 21:58:38 -0800

    visualize adapted to new type

--- src/ContinuedFraction.jl
+++ src/ContinuedFraction.jl
@@ -4,6 +4,8 @@
 
     [1 2 3 4] = 1 + 1/(2+1/(3+(1/4)))
 
+    NOTE the first term is the whole part.
+
     a fun visualization function is the first function you can use here for fun.
 
     but wait... we can have repeats
@@ -20,6 +22,11 @@
     This is not possible because it would clash with [ 1 2 ; 3 4 ]
     being a Matrix constructor.
 
+    TODO Float(cf::ContinuedFraction) needs more test coverage
+    TODO FLoat(π) and other irrationals
+    TODO ContinuedFraction(Float) :)
+    TODO whole parts for everything...
+    TODO Rational with orbits?
     TODO this needs to be its own package.
     TODO this needs to be prominently displayed.
     TODO professional looking juliadoc
@@ -55,7 +62,8 @@ struct ContinuedFraction{T<:Vector{Int}} <: Real
 end
 
 Base.:(==)(cf1::ContinuedFraction, cf2::ContinuedFraction) =
- cf1.head == cf2.head && cf1.orbit == cf2.orbit
+    cf1.head == cf2.head &&
+    cf1.orbit == cf2.orbit
 
 function ContinuedFraction(q::Rational)
     #=
@@ -94,7 +102,6 @@ function ContinuedFraction(q::Rational)
     return ContinuedFraction(h)
 end
 
-
 # uh, cuz why not. Of course we might want to write these values to disk.
 function read(s::IO)
     head = read(s)
@@ -106,92 +113,20 @@ function write(s::IO, cf::ContinuedFraction)
     write(s, head(cf), orbit(cf))
 end
 
-function visualize(cf::Vector{Int})::String
-    # TODO this needs to be adapted... badly
+function visualize(cf::ContinuedFraction)::String
     # This is one of the coolest things I have ever discovered.
-    return join(cf, " + 1/(") * ")" ^ (length(cf) - 1)
+    if terminates(cf)
+        return join(cf.head, " + 1/(") *
+            ")" ^ (length(cf.head) - 1)
+    else
+        return join(cf.head, " + 1/(") *
+            " + 1/(" *
+            join(cf.orbit, " + 1/(") *
+            " + ... " *
+            ")" ^ (length(cf.head)+length(cf.orbit)-1)
+    end
 end
 
 period(cf::ContinuedFraction)::Int = length(cf.orbit)
 terminates(cf::ContinuedFraction)::Bool = cf.orbit == Vector{Int}()
 Base.length(cf::ContinuedFraction)::Int = Inf ? length(cf.orbit) > 0 : length(cf.head)
-
-function cf_to_float(cf::Vector; whole_part::Int=0)::Float
-	#=
-    Converts a cf to a float.
-
-    Okay, this was something we apperenly only glanced the surface of
-    all those years ago. This is actually something very cool.
-
-    Consider a cf with orbit 2 [c1 c2 ;].
-
-    Thus, x = 1/(c1 + 1/(c2 + x))
-    Which equates to c1 x^2 + (c1c2 - 1)x + 1 - c2 = 0
-    which is a quadratic equation.
-
-    If fact, all of these setup quadratic equations to be solved.
-    The problem is, the quadratic equations get nastier and nastier as the
-    orbits increase.
-
-    So that is not a feasible solution. (its a recurrence relation)
-    So what we do instead is cheese it in a very unelegant manner
-
-    []
-
-    -----
-    Here is the original documentation kept for nostalgiac purposes:
-
-	Takes a continued fraction representing an irrational
-	number and converts it to an irrational number.
-	Remember, all irrational numbers have periodic continued fractions.
-
-	There are two parts.. the first part is collapsing the cf.
-
-	We have x = 1/(c[1] + 1/(cf[2] + 1/(cf[3] + x))
-	for a cf with an orbit of 3.
-
-	Once that is collapsed, it will give us
-	x = (a1 + b1x)/(a2 + b2x)
-
-	Which yields...
-	b2x^2 + (a2 - b1)x - a1 = 0
-
-	That can be solved using quadratic formula.
-	We are only interested in the plus part of the answer.
-
-	TODO
-	HAS NOT BEEN TESTED THOROUGHLY.
-    TODO i completely borked this
-	=#
-
-	# numerator constant and coefficient of x
-	a1 = 1
-    b1 = 0
-	# denominator constant and coefficient of x
-	a2 = cf[end]
-    b2 = 1
-	for i in length(cf)-1:-1:1
-		a1 = cf[i]*a2 + a1
-        b1 = cf[i]*b2 + b1
-		
-		# flip the fraction over by swapping
-		temp = a1
-		a1 = a2
-		a2 = temp
-		temp = b1
-		b1 = b2
-		b2 = temp
-	end
-
-	# set up and solve our quadratic equation.
-    # TODO this algo seems to require the below function to have a specific
-    # return style... but it does no longer. That is why i added testing :)
-	result = solve_quadratic(b2, a2-b1, -a1, formatted=true, sol_type="plus")
-
-	# Whole part, add it to the result. So result[1] will increase
-	# by whole_part*result[2]
-	if whole_part > 0
-		result[1] = whole_part*result[2]
-	end
-	return result
-end
